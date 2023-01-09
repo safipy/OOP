c5c3b86daee8df95c022371ebb2f58112293530c
safipy <122204038+safipy@users.noreply.github.com>
2023-01-09 21:15:45 +0600

    Add files via upload

--- Hero.py
+++ Hero.py
@@ -13,11 +13,13 @@ class SuperHero():
 
     def __len__(self):
         return f'{self.catchphrase}'
-def hero_name(name):
-    print(f'Hero name is {name} ')
 
 
-hero_name('Peter Parker')
+    def hero_name(name):
+       print(f'Hero name is {name} ')
+
+
+    hero_name('Peter Parker')
 
 
 def hero_points(health_points):
@@ -27,8 +29,56 @@ def hero_points(health_points):
 
 hero_points(50)
 
-hero = SuperHero('Peter Parker', 'Spider-Man', 'Strong', 100,'With great power comes great responsibility')
+hero = SuperHero('Peter Parker', 'Spider-Man', 'Strong', 100,
+                 'With great power comes great responsibility')
 print(f' Hero nickname is {hero.nickname}.'
       f' \n He is very {hero.superpower}. '
       f'\n And his health-point is = {hero.health_points}')
 print(f' His cathphrase len is = {len(hero.catchphrase)}')
+
+
+class HeroOne(SuperHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly=False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = fly
+
+    def hero_points(self):
+      health = self.health_points ** 2
+      print(f'Hero health = {health}')
+      self.fly = True
+
+    def true_phrase(self):
+       print(f' fly in the True_phrase ')
+aquaman = HeroOne('Artur,', 'Aquaman,', 'breathe underwater,', 50, 'You have to do what you feel is right, even if it hurts you.', 'n')
+print(aquaman.fly)
+aquaman.true_phrase
+hero_points(aquaman.health_points)
+
+class HeroTwo(SuperHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly=False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = fly
+    def hero_points(self):
+      health = self.health_points ** 2
+      print(f'Hero health = {health}')
+      self.fly = True
+
+    def true_phrase(self):
+        print(f' fly in the True_phrase ')
+
+tor = HeroTwo(f'Thor', f'Thor','lightning control', 50,\
+               'Feel free to choose expressions. Loki is wrong, but he is still an Asgardian and also my brother', 5)
+print(tor)
+tor.true_phrase
+hero_points(tor.health_points)
+class Villain(HeroTwo):
+    people = 'monster'
+    def gen_x(self):
+        pass
+    def crit(self):
+        self.damage **= 2
+        return self.damage
+
+print(HeroTwo.damage)
